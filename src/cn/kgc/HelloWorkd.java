ce5b6e37f135954675a75235c915e528813af894
yangyineng123 <3167152351@qq.com>
2020-09-04 18:02:23 +0800

    HelloWorld

---
+++ src/cn/kgc/HelloWorkd.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+public class HelloWorkd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
